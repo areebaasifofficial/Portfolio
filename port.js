0a515fd891fae96a38f269f05aa59288031cf416
areeba1606 <dimpleq750@gmail.com>
2025-09-16 10:20:10 -0700

    Add files via upload

---
+++ port.js
@@ -0,0 +1,5 @@
+const cursor=document.querySelector('.cursor');
+    document.addEventListener('mousemove', e => {
+      cursor.style.top = e.clientY + 'px';
+      cursor.style.left = e.clientX + 'px';
+    });
