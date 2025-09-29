cac9c1a6d89a7fafb1360b5825c389d7b2af3315
areeba1606 <dimpleq750@gmail.com>
2025-09-29 09:16:32 +0500

    allow changes

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const cursor=document.querySelector('.cursor');
-    document.addEventListener('mousemove', e => {
-      cursor.style.top = e.clientY + 'px';
-      cursor.style.left = e.clientX + 'px';
-    });
+// Glowing Curser
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', e => {
+  cursor.style.top = e.clientY + 'px';
+  cursor.style.left = e.clientX + 'px';
+});
+// Get elements
+const popup = document.getElementById("popup");
+const freelancerBtn = document.getElementById("freelancerBtn");
+const closePopup = document.getElementById("closePopup");
+
+// Show popup
+freelancerBtn.addEventListener("click", () => {
+  popup.style.display = "block";
+});
+
+// Hide popup
+closePopup.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
+// Hide when clicking outside
+// window.addEventListener("click", (e) => {
+//   if (e.target === popup) {
+//     popup.style.display = "none";
+//   }
+// });
